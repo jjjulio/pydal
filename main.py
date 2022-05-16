7fdf5f845a8eb2e648c589ef4b988ba3b8fbb3d7
jjjulio <jjulio.medina.9@gmail.com>
2022-05-15 22:17:34 -0500

    B 0.1 Downloads artists/albums/tracks with all tags

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from time import sleep
 
 try:
     pydal = Pydal()
-    artist = pydal.getArtist(7674158)
+    #artist = pydal.getArtist(7674158)
     artist = pydal.getArtist(24674323)
     artist.download()
     pydal.close()
