19ee8437a28614ff5b4b2f2f5d8d748534c1ecc3
jjjulio <jjulio.medina.9@gmail.com>
2022-05-16 10:03:53 -0500

    B 0.1 Download new releases from feed

--- updates.py
+++ updates.py
@@ -6,6 +6,7 @@ from time import sleep
 try:
     pydal = Pydal()
     albums = pydal.getFeed()
+    print("Downloading new releases!")
     for album in albums:
         album.download()
     pydal.close()
