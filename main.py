0b2f13b3280f287277d6d9e5769650ad1b9d06e5
jjjulio <jjulio.medina.9@gmail.com>
2022-05-15 22:20:27 -0500

    B 0.1 Added Library to Plex and Scans after album download

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from time import sleep
 
 try:
     pydal = Pydal()
-    #artist = pydal.getArtist(7674158)
-    artist = pydal.getArtist(5271845)
+    artist = pydal.getArtist(7674158)
+    #artist = pydal.getArtist(5271845)
     artist.download()
     pydal.close()
 except KeyboardInterrupt:
