5b94549114c7c2cec583c111f45ed20cab70f24b
jjjulio <jjulio.medina.9@gmail.com>
2022-07-07 19:27:08 -0500

    B 1.0 Addede Release type Album

--- Pydal.py
+++ Pydal.py
@@ -158,8 +158,11 @@ class Pydal(object):
             paras = {'countryCode': self.user['user']['countryCode'], 'limit': 1}
             header = {'authorization': 'Bearer {}'.format(self.user['access_token'])}
             response = requests.get(url, headers=header, params=paras).json()
-            if "not found" in response['userMessage']:
-                return self.getAlbum(119187177)
+            try:
+                if "not found" in response['userMessage']:
+                    return self.getAlbum(119187177)
+            except:
+                pass
             with open('./DB/albums/' + str(albumId) + '.json', 'w') as outfile:
                 json.dump(response, outfile)
         return Album(response)
